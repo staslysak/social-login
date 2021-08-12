200d5e1756aea7c5b2779290f7a1314820c4505d
Stas Lysak <staslyzak@gmail.com>
2021-08-12 15:08:14 +0300

    Update popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,62 +1,60 @@
-const getPopupPositionProperties = ({ width = 600, height = 740 } = {}) => {
-    return Object.entries({
-        left: window.screen.width / 2 - width / 2,
-        top: window.screen.height / 2 - height / 2,
-        width,
-        height,
-    })
-        .map(([key, value]) => `${key}=${value}`)
-        .join(',');
-};
+// import qs from 'query-string'
 
-export class _Popup {
-    close() {
-        this.popup.close();
-        this.popup = null;
-    }
-
-    open({ uri, from = 'OAUTH_REDIRECT', onSuccess, onFailure }) {
-        this.uri = uri;
-        this.from = from;
-        this.onSuccess = onSuccess;
-        this.onFailure = onFailure;
-
-        if (this.popup) {
-            return this.close();
-        }
-
-        const popup = window.open(
-            this.uri,
-            '_blank',
-            getPopupPositionProperties()
-        );
-        this.popup = popup;
-
-        window.removeEventListener('message', this.receiveMessage, false);
-        window.addEventListener('message', this.receiveMessage, false);
-    }
-
-    receiveMessage = ({ origin, data }) => {
-        // const { state } = qs.parse(this.uri.split('?')[1]);
-
-        if (origin === window.location.origin) {
-            // Prevent CSRF attack by testing state
-            // if (state && data.state !== state) {
-            //     this.popup && this.popup.close();
-            //     return;
-            // }
-
-            if (data.error && data.from === this.from) {
-                this.onFailure(data);
-            } else if (data.code && data.from === this.from) {
-                this.onSuccess(data);
-            }
-
-            if (this.popup) {
-                this.close();
-            }
-        }
-    };
+const getPopupPositionProperties = ({width = 600, height = 740} = {}) => {
+  return Object.entries({
+    left: window.screen.width / 2 - width / 2,
+    top: window.screen.height / 2 - height / 2,
+    width,
+    height,
+  })
+    .map(([key, value]) => `${key}=${value}`)
+    .join(',')
 }
 
-export const Popup = new _Popup();
+export class WindowPopup {
+  close() {
+    this.popup.close()
+    this.popup = null
+  }
+
+  open({uri, from = 'OAUTH_REDIRECT', onSuccess, onFailure}) {
+    this.uri = uri
+    this.from = from
+    this.onSuccess = onSuccess
+    this.onFailure = onFailure
+
+    if (this.popup) {
+      return this.close()
+    }
+
+    const popup = window.open(this.uri, '_blank', getPopupPositionProperties())
+    this.popup = popup
+
+    window.removeEventListener('message', this.receiveMessage, false)
+    window.addEventListener('message', this.receiveMessage, false)
+  }
+
+  receiveMessage = ({origin, data}) => {
+    // const { state } = qs.parse(this.uri.split('?')[1]);
+
+    if (origin === window.location.origin) {
+      // Prevent CSRF attack by testing state
+      // if (state && data.state !== state) {
+      //     this.popup && this.popup.close();
+      //     return;
+      // }
+
+      if (data.error && data.from === this.from) {
+        this.onFailure(data)
+      } else if (data.code && data.from === this.from) {
+        this.onSuccess(data)
+      }
+
+      if (this.popup) {
+        this.close()
+      }
+    }
+  }
+}
+
+export const Popup = new WindowPopup()
