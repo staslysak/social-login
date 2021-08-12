f97988dd3d2f29e279322282d796b9c4195e7ecd
Stas Lysak <staslyzak@gmail.com>
2021-08-12 15:09:46 +0300

    Update social-login.js

--- src/social-login.js
+++ src/social-login.js
@@ -1,69 +1,66 @@
-import qs from 'query-string';
+import qs from 'query-string'
 
-import { Popup } from './popup';
-import { getSocialUri } from './oauth-uri';
+import {Popup} from './popup'
+import {getSocialUri} from './oauth-uri'
 
 export const handleRedirect = (from = 'OAUTH_REDIRECT') => {
-    const {
-        error,
-        errorMessage = 'Login failed. Please try again.',
-        state,
-        code,
-    } = qs.parse(window.location.search);
+  const {
+    code,
+    state,
+    error,
+    errorMessage = 'Login failed. Please try again.',
+  } = qs.parse(window.location.search)
 
-    if (error || code) {
-        if (window.opener) {
-            window.opener.postMessage(
-                {
-                    ...(error
-                        ? {
-                              state,
-                              error,
-                              errorMessage,
-                          }
-                        : {
-                              state,
-                              code,
-                          }),
-                    from,
-                },
-                window.location.origin
-            );
-        }
+  // Close tab if user cancelled login
+  if (error === 'user_cancelled_login') {
+    window.close()
+    return
+  }
 
-        // Close tab if user cancelled login
-        if (error === 'user_cancelled_login') {
-            window.close();
-        }
+  if (window.opener && (error || code)) {
+    const message = {
+      from,
+      state,
+      ...(error
+        ? {
+            error,
+            errorMessage,
+          }
+        : {
+            code,
+          }),
     }
-};
+
+    window.opener.postMessage(message, window.location.origin)
+  }
+}
 
 export const handleSocialLogin = ({
-    provider,
+  provider,
 
-    scope,
-    clientId,
-    redirectUri,
-    state,
+  scope,
+  clientId,
+  redirectUri,
+  state,
 
-    onSuccess = () => {},
-    onFailure = () => {},
+  onSuccess = () => {},
+  onFailure = () => {},
 }) => {
-    const handleLogin = () => {
-        const uri = getSocialUri({
-            provider,
-            scope,
-            clientId,
-            redirectUri,
-            state: state || 'STATE',
-        });
+  const handleLogin = () => {
+    const uri = getSocialUri({
+      provider,
+      scope,
+      clientId,
+      redirectUri,
+      state: state || 'STATE',
+    })
 
-        Popup.open({
-            uri,
-            onSuccess,
-            onFailure,
-        });
-    };
+    Popup.open({
+      uri,
+      onSuccess,
+      onFailure,
+    })
+  }
 
-    return handleLogin;
-};
+  return handleLogin
+}
