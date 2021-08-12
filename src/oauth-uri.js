8177da772bd63369428bc1bc238838fefb731cf7
Stas Lysak <staslyzak@gmail.com>
2021-08-12 15:07:08 +0300

    Update oauth-uri.js

--- src/oauth-uri.js
+++ src/oauth-uri.js
@@ -1,89 +1,88 @@
-import qs from 'query-string';
+import qs from 'query-string'
 
 const getFacebookUri = ({
-    scope = 'public_profile,email',
-    clientId,
-    redirectUri = '',
-    state,
+  scope = 'public_profile,email',
+  clientId,
+  redirectUri = '',
+  state,
 }) => {
-    const baseUri = 'https://www.facebook.com/v10.0/dialog/oauth';
-    const search = qs.stringify({
-        client_id: clientId,
-        scope,
-        redirect_uri: redirectUri,
-        state,
-    });
+  const baseUri = 'https://www.facebook.com/v10.0/dialog/oauth'
+  const search = qs.stringify({
+    client_id: clientId,
+    scope,
+    redirect_uri: redirectUri,
+    state,
+  })
 
-    return `${baseUri}?${search}`;
-};
+  return `${baseUri}?${search}`
+}
 
 const getGithubUri = ({
-    scope = 'user:email',
-    clientId,
-    redirectUri = '',
-    state,
+  scope = 'user:email',
+  clientId,
+  redirectUri = '',
+  state,
 }) => {
-    const baseUri = 'https://github.com/login/oauth/authorize';
-    const search = qs.stringify({
-        client_id: clientId,
-        scope,
-        redirect_uri: redirectUri,
-        state,
-    });
+  const baseUri = 'https://github.com/login/oauth/authorize'
+  const search = qs.stringify({
+    client_id: clientId,
+    scope,
+    redirect_uri: redirectUri,
+    state,
+  })
 
-    return `${baseUri}?${search}`;
-};
+  return `${baseUri}?${search}`
+}
 
 const getGoogleUri = ({
-    scope = 'userinfo.email,userinfo.profile',
-    clientId,
-    redirectUri = '',
-    state,
+  scope = 'userinfo.email,userinfo.profile',
+  clientId,
+  redirectUri = '',
+  state,
 }) => {
-    const baseUri = 'https://accounts.google.com/o/oauth2/v2/auth';
-    const search = qs.stringify({
-        include_granted_scopes: true,
-        response_type: 'code',
-        access_type: 'offline',
-        client_id: clientId,
-        scope: scope
-            .split(',')
-            .map((s) => `https://www.googleapis.com/auth/${s}`)
-            .join(' '),
-        redirect_uri: redirectUri,
-        state,
-    });
+  const baseUri = 'https://accounts.google.com/o/oauth2/v2/auth'
+  const search = qs.stringify({
+    include_granted_scopes: true,
+    response_type: 'code',
+    access_type: 'offline',
+    client_id: clientId,
+    scope: scope
+      .split(',')
+      .map((s) => `https://www.googleapis.com/auth/${s}`)
+      .join(' '),
+    redirect_uri: redirectUri,
+    state,
+  })
 
-    return `${baseUri}?${search}`;
-};
+  return `${baseUri}?${search}`
+}
 
 const getLinkedInUri = ({
-    scope = 'r_liteprofile,r_emailaddress',
-    clientId,
-    redirectUri = '',
-    state,
+  scope = 'r_liteprofile,r_emailaddress',
+  clientId,
+  redirectUri = '',
+  state,
 }) => {
-    const baseUri = 'https://www.linkedin.com/oauth/v2/authorization';
-    const search = qs.stringify({
-        response_type: 'code',
-        client_id: clientId,
-        scope: scope
-            .split(',')
-            .map((s) => encodeURI(s))
-            .join(' '),
-        redirect_uri: redirectUri,
-        state,
-    });
+  const baseUri = 'https://www.linkedin.com/oauth/v2/authorization'
+  const search = qs.stringify({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope
+      .split(',')
+      .map((s) => encodeURI(s))
+      .join(' '),
+    redirect_uri: redirectUri,
+    state,
+  })
 
-    return `${baseUri}?${search}`;
-};
+  return `${baseUri}?${search}`
+}
 
 const providers = {
-    github: getGithubUri,
-    google: getGoogleUri,
-    facebook: getFacebookUri,
-    linkedin: getLinkedInUri,
-};
+  github: getGithubUri,
+  google: getGoogleUri,
+  facebook: getFacebookUri,
+  linkedin: getLinkedInUri,
+}
 
-export const getSocialUri = ({ provider, ...props }) =>
-    providers[provider](props);
+export const getSocialUri = ({provider, ...props}) => providers[provider](props)
